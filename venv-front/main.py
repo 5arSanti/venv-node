cd89f1fc538ee4894f486b5058118f31ea94b787
5arSanti <santiari05@hotmail.com>
2025-04-12 00:21:32 -0500

    Refactor Selenium script to use WebDriverWait for element presence; streamline code by removing unnecessary lines and adding options for headless execution

--- venv-front/main.py
+++ venv-front/main.py
@@ -2,38 +2,35 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=options)
 
-driver.get("http://localhost:5173")
+try:
+    driver.get("http://localhost:5173")
 
-name_input = driver.find_element(By.ID, "name")
+    wait = WebDriverWait(driver, 10)
+    name_input = wait.until(EC.presence_of_element_located((By.ID, "name")))
+    last_name_input = wait.until(EC.presence_of_element_located((By.ID, "last_name")))
+    email_input = wait.until(EC.presence_of_element_located((By.ID, "email")))
+    phone_input = wait.until(EC.presence_of_element_located((By.ID, "phone")))
 
-last_name_input = driver.find_element(By.ID, "last_name")
+    name_input.send_keys("John")
+    last_name_input.send_keys("Doe")
+    email_input.send_keys("test@mail.com")
+    phone_input.send_keys("1234567890")
 
-email_input = driver.find_element(By.ID, "email")
+    submit_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@type="submit"]')))
+    submit_button.click()
 
-phone_input = driver.find_element(By.ID, "phone")
+    success_message = wait.until(EC.presence_of_element_located((By.ID, 'mensaje-exito')))
+    assert "Exitoso" in success_message.text
 
-
-name_input.send_keys("John")
-
-last_name_input.send_keys("Doe")
-
-email_input.send_keys("test@mail.com")
-
-phone_input.send_keys("1234567890")
-
-submit_button = driver.find_element(By.XPATH, '//button[@type="submit"]')
-
-submit_button.click()
-
-time.sleep(2)
-
-success_message = driver.find_element(By.ID, 'mensaje-exito')
-
-assert "Exitoso" in success_message.text
-
-driver.quit()
+finally:
+    driver.quit()
